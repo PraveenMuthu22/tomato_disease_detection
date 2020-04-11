c655ba7ed606ba3f22f31c3731757f3de01e045d
Praveen Muthukumarana <praveenmuth2@gmail.com>
2020-04-11 15:28:22 +0530

    aspect_aware_preprocessor changed

--- utils/preprocessors/aspect_aware_preprocessor.py
+++ utils/preprocessors/aspect_aware_preprocessor.py
@@ -6,12 +6,12 @@ class AspectAwarePreprocessor:
     CONTRUCTOR
     witdh : desired width
     height : desired height
-    inter : interpolation method used when resizing the image
+    interpolation : for resizing image
     """
-    def __init__(self,width,height,inter=cv2.INTER_AREA):
+    def __init__(self,width,height,interpolation=cv2.INTER_AREA):
         self.width = width
         self.height = height
-        self.inter = inter
+        self.interpolation = interpolation
 
     """
     image : image to be preprocessed
@@ -19,26 +19,23 @@ class AspectAwarePreprocessor:
     def preprocess(self,image):
         # Get wdith and height of image
         (h, w) = image.shape[:2]
-        dW = 0
-        dH = 0
+        dWeight = 0
+        dHeight = 0
 
-        # if width is the shorter dimension, resize image by width and crop height
+        # if width shorter, resize based on width and crop height
         if w < h:
             image = imutils.resize(image, width=self.width,
-                                   inter=self.inter)
-            dH = int((image.shape[0] - self.height) / 2.0)
+                                   interpolation=self.interpolation)
+            dHeight = int((image.shape[0] - self.height) / 2.0)
 
-        # if height is the shorter dimension, resize image by height and crop width
+        # if height shorter, resize based on height and crop width
         else:
             image = imutils.resize(image, height=self.height,
-                               inter=self.inter)
-            dW = int((image.shape[1] - self.width) / 2.0)
+                               interpolation=self.interpolation)
+            dWeight = int((image.shape[1] - self.width) / 2.0)
 
-        # re-grab the width and height and use the deltas to crop the center of the image:
         (h, w) = image.shape[:2]
-        image = image[dH:h - dH, dW:w - dW]
+        image = image[dHeight:h - dHeight, dWeight:w - dWeight]
 
-        # our image target image dimensions may be off by ± one pixel; therefore, we make a call to cv2.resize to 
-        # ensure our output image has the desired width and height.
         return cv2.resize(image, (self.width, self.height),
-                          interpolation=self.inter)
+                          interpolation=self.interpolation)
